fcda1cc55c4eea027fa3ba67f8296d931410b032
lyuxc <lyuxc1893@163.com>
2020-12-16 23:15:58 +0800

    up

--- day01/demo35_ex.py
+++ day01/demo35_ex.py
@@ -1,8 +1,25 @@
-# coding=utf-8
+ # coding=utf-8
 if __name__ == '__main__':
     def factorial(num):
-        
-        print('factorial:', num)
+        s = 1
+
+        # for方法
+        # num +=1
+        # for tmp in range(1,num):
+        #     s *= tmp
+        # return s
+
+        # while
+        fact = 1
+        while True:
+            if s < num:
+                s += 1
+                fact *= s
+            else:
+                return fact
+                break
 
     inputNum = int(input("输入阶乘数："))
-    factorial(inputNum)
+    rs = factorial(inputNum)
+    print('阶乘结果：', rs)
+    
